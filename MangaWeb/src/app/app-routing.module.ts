60d4c88091da22e17c38677d0b1d449d3979f556
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2023-03-20 08:30:26 +0000

    punto 2

--- MangaWeb/src/app/app-routing.module.ts
+++ MangaWeb/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemMangaComponent } from './item-manga/item-manga.component';
+import { SearchMangaComponent } from './search-manga/search-manga.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: SearchMangaComponent} ,
+  {path:'item-manga',component: ItemMangaComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MangaWeb/src/app/app.module.ts
+++ MangaWeb/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchMangaComponent } from './search-manga/search-manga.component';
+import { ItemMangaComponent } from './item-manga/item-manga.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMangaComponent,
+    ItemMangaComponent
   ],
   imports: [
     BrowserModule,
